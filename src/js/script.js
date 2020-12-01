89f01d172e4bfa39a420d72a6629928a7dc6c977
Anya <likamoon@list.ru>
2020-12-02 02:23:29 +0300

    Шапка + функционал

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {    
 
+    //Бургер
+    var isMobile = window.matchMedia("(max-width: 1200px)").matches;
+    var burger = document.querySelector('#burger');
+    if (burger) {
+        burger.addEventListener('click', toggleBurger);
+
+        var menuBlock = document.querySelector('.menu');
+
+        function toggleBurger(event) {
+            event.preventDefault();
+            $(menuBlock).slideToggle(300);
+            burger.classList.toggle('burger--open');
+        }
+
+        document.addEventListener('click', closeBurger);
+        function closeBurger(event) {
+            if (!(event.target.closest('.menu')) && !(event.target.closest('#burger'))) {
+                $(menuBlock).slideUp(300);
+                burger.classList.remove('burger--open');
+            }
+        }
+    }
+
+    // Подменю на моб. версии
+    var btnOpenSubmenuArr = document.querySelectorAll('.menu__link-more');
+    if(btnOpenSubmenuArr.length !== 0) {
+        for(var i=0; i< btnOpenSubmenuArr.length; i++) {
+            btnOpenSubmenuArr[i].addEventListener('click', openSubMenu);
+            function openSubMenu(event) {
+                event.preventDefault();
+                $(this.closest('.menu__item').querySelector('.submenu')).slideToggle(300);
+                this.classList.toggle('menu__link-more--open')
+            }
+        }
+    }
+
 })
 
 /*Полифилы для ie*/
--- www/js/script.js
+++ www/js/script.js
@@ -1,6 +1,44 @@
 "use strict";
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  //Бургер
+  var isMobile = window.matchMedia("(max-width: 1200px)").matches;
+  var burger = document.querySelector('#burger');
+
+  if (burger) {
+    var toggleBurger = function toggleBurger(event) {
+      event.preventDefault();
+      $(menuBlock).slideToggle(300);
+      burger.classList.toggle('burger--open');
+    };
+
+    var closeBurger = function closeBurger(event) {
+      if (!event.target.closest('.menu') && !event.target.closest('#burger')) {
+        $(menuBlock).slideUp(300);
+        burger.classList.remove('burger--open');
+      }
+    };
+
+    burger.addEventListener('click', toggleBurger);
+    var menuBlock = document.querySelector('.menu');
+    document.addEventListener('click', closeBurger);
+  } // Подменю на моб. версии
+
+
+  var btnOpenSubmenuArr = document.querySelectorAll('.menu__link-more');
+
+  if (btnOpenSubmenuArr.length !== 0) {
+    for (var i = 0; i < btnOpenSubmenuArr.length; i++) {
+      var openSubMenu = function openSubMenu(event) {
+        event.preventDefault();
+        $(this.closest('.menu__item').querySelector('.submenu')).slideToggle(300);
+        this.classList.toggle('menu__link-more--open');
+      };
+
+      btnOpenSubmenuArr[i].addEventListener('click', openSubMenu);
+    }
+  }
+});
 /*Полифилы для ie*/
 
 if (!Element.prototype.matches) {
